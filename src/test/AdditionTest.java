a436003598f0cb78607458b1de9bb6fee241bb2c
swarnaburra <swarnaburra2025@gmail.com>
2026-01-06 00:31:27 -0600

    *Added Unit Testing Checklist into the code.

--- src/test/AdditionTest.java
+++ src/test/AdditionTest.java
@@ -1,3 +1,11 @@
+//The test class mirrors the name of the class being tested (with Test at the
+//        end)
+//The test method name clearly describes what is being tested
+//The test can run independently and doesn’t rely on other tests
+//There is at least one test for the normal (“happy path”) case
+//There is at least one test for an edge case or unusual input
+//The test uses appropriate assertions to verify behavior
+
 package test;
 
 import main.java.Addition;
